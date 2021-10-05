9acac059f705755d6f1d93e61f60051af89c455a
Tim Houtman <0303927@student.rocvantwente.nl>
2021-10-05 11:42:15 +0200

    Helper class for roles added to the project

---
+++ Applicatie/CCSB/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CCSB.Utility;
+
+namespace CCSB.Models
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
