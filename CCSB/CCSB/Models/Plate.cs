a2255b64710674e8166f6b454b3d17bb36ac0e3f
[dylanhagmolen] <0318584@student.rocvantwente.nl>
2021-10-26 09:19:42 +0200

    Form

---
+++ CCSB/CCSB/Models/Plate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCSB.Models
+{
+    public class Plate
+    {
+        [Key]
+        public int InvoiceNumber { get; set; }
+        public string Type { get; set; }
+        public string LicensePlate { get; set; }
+        public int Length { get; set; }
+        public int AmountPerMeter { get; set; }
+    }
+}
---
+++ CCSB/CCSB/Models/invoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CCSB.Models
+{
+    public class invoice
+    {
+        [Key]
+        public int InvoiceNumber { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public DateTime ExperationDate { get; set; }
+    }
+}
